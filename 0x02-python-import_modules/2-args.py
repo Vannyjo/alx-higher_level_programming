4036e885848a9ba13211fee0a47fc6120f56da9e
Vannyjo <ezejoseph96@gmail.com>
2023-08-14 17:45:50 -0700

    importing maths calc into mould

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,19 +2,12 @@
 
 import sys
 
-if __name__ == "__main__":
-    argv = sys.argv
-    argc = len(argv) - 1
-
-    if argc == 0:
-        print("Number of argument(s): 0.")
-        print(".")
-    else:
-        if argc == 1:
-            print("Number of argument(s): 1: {}.".format(argv[1]))
-            print("1: {}.".format(argv[1]))
-        else:
-            print("Number of argument(s): {}: {}.".format(argc, ' '.join(argv[1:])))
-            for i, arg in enumerate(argv[1:], start=1):
-                print("{}: {}.".format(i, arg))
-
+count = len(sys.argv) - 1
+if count == 0:
+    print("0 arguments.")
+elif count == 1:
+    print("1 argument:")
+else:
+    print("{} arguments:".format(count))
+for i in range(count):
+     print("{}: {}".format(i + 1, sys.argv[i + 1]))
