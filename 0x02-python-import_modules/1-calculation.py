b2587e66d2b7b99fe194b1ade246e06748a04378
Vannyjo <ezejoseph96@gmail.com>
2023-08-14 17:30:18 -0700

    importing maths calc into mould

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    from calculator_1 import add, multiply, divide, substract
+    from calculator_1 import add, substract, multiply, divide
+
     a = 10
     b = 5
+
     print("{} + {} = {}".format(a, b, add(a, b)))
+    print("{} - {} = {}".format(a, b, substract(a, b)))
     print("{} * {} = {}".format(a, b, multiply(a, b)))
     print("{} / {} = {}".format(a, b, divide(a, b)))
-    print("{} - {} = {}".format(a, b, substract(a, b)))
