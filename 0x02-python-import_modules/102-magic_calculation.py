bb3a80bcfb52d4b4c0cd123a6b1024ad51b808c1
Vannyjo <ezejoseph96@gmail.com>
2023-08-16 12:07:42 -0700

    hot fix

---
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+def magic_calculation(a, b):
+    """Match bytecode provided"""
+    from magic_calculation_102 import add, sub
+
+    if i < j:
+        k = add(i, j)
+        for f in range(4, 6):
+            k = add(k, f)
+        return (k)
+
+    else:
+        return (sub(i, j))
