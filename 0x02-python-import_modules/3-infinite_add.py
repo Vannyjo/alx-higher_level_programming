97684a833cc115daa9c30b51d89848d88b041dbd
Vannyjo <ezejoseph96@gmail.com>
2023-08-16 11:30:28 -0700

    hot fix

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,12 +1,9 @@
 #!/usr/bin/python3
+if __name__ == "__main__":
 import sys
 
 def infinite_add():
-    # Sum up all the arguments after converting them to integers
     result = sum(int(arg) for arg in sys.argv[1:])
     print(result)
 
-# Ensure the code isn't executed when the module is imported
-if __name__ == "__main__":
     infinite_add()
-
