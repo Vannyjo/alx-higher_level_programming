ce07a25b32f2bb640a7219e464b01b038724c020
Vannyjo <ezejoseph96@gmail.com>
2023-08-16 11:19:52 -0700

    hot fix

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
+import sys
 
 if __name__ == "__main__":
-    import sys
+    num_args = len(sys.argv) - 1  # subtracting 1 to exclude the script name itself
 
-    count = len(sys.argv) - 1
-    if count == 0:
+    if num_args == 0:
         print("0 arguments.")
-    elif count == 1:
+    elif num_args == 1:
         print("1 argument:")
     else:
-         print("{} arguments:".format(count))
-    for i in range(count):
-        print("{}: {}".format(i + 1, sys.argv[i + 1]))
+        print("{} arguments:".format(num_args))
+
+    for i in range(1, len(sys.argv)):
+        print("{}: {}".format(i, sys.argv[i])
