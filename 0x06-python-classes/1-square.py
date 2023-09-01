02ad4a370456ba82f41a634d123d5f11a26d3a89
Vannyjo <ezejoseph96@gmail.com>
2023-09-01 13:23:41 -0700

    task 0

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,15 +1,12 @@
 #!/usr/bin/python3
-""" the shibang declearation
-"""
+"""Defines a square """
+
 class Square:
-    """defining the class named Size"""
+     """Represents a square"""
 
     def __init__(self, size):
-        """initializing the arrtributes of the class square with
-        a self attribute being the base"""
-
-        pass
-    """tells the program to pass"""
+        """Initializing this square class
+        Args: size - represnets the size of the square defined
+        """
 
     self.__size = size
-    """setting the size attribute to a private attribute"""
