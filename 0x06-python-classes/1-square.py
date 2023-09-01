affb3269626b6a41b4eb1a4ecf57448cf8226086
Vannyjo <ezejoseph96@gmail.com>
2023-09-01 13:29:23 -0700

    task 0

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,16 +1,13 @@
-    #!/usr/bin/python3
-"""Square class definition"""
+#!/usr/bin/python3
+"""Defines a square """
 
 
 class Square:
-    """Represents a square
-    Attributes:
-        __size (int): size of a side of the square
-    """
+    """Represents a square"""
+
     def __init__(self, size):
-        """Initializes a square
-        Args:
-            size (int): size of a side of the square
-        Returns: None
+        """Initializing this square class
+        Args: size - represnets the size of the square defined
         """
+
         self.__size = size
