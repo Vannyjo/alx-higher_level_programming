59cdca6c4278603b1a3df200e9cbb02716732bd7
Vannyjo <ezejoseph96@gmail.com>
2023-09-13 12:46:16 -0700

    task 6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Contains a base class named BaseGeometry."""
+
+
+class BaseGeometry:
+    """this class defines a class BaseGeometry"""
+
+    def area(self):
+        """raises an exception method not implemented yet"""
+        raise Exception("area() is not implemented")
